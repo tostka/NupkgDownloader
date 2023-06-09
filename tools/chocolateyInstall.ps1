3f5ad8c1d255e66eebeb0ba7f88635f75ce8001a
Colavito, Brian <brian.colavito@verint.com>
2023-06-08 23:56:35 -0400

    no

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -6,11 +6,3 @@ $AllUsersModuleDir = Join-Path $env:ProgramFiles "/PowerShell/Modules/NupkgDownl
 New-Item $AllUsersModuleDir/src -ItemType Directory -Force | Out-Null
 Copy-Item $PackageDir/src/* $AllUsersModuleDir/src
 Copy-Item $PackageDir/NupkgDownloader.ps* $AllUsersModuleDir
-
-# In case we are manually installing, we replace the version token with a dummy number.
-
-$versionToken = "#{MajorMinorPatch}#"
-$psd = Get-Content $AllUsersModuleDir/NupkgDownloader.psd1 -Raw
-if ($psd.Contains($versionToken)) {
-	$psd -Replace $versionToken,"1.0" | Set-Content $AllUsersModuleDir/NupkgDownloader.psd1
-}
